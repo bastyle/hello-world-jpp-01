1482852625670833940a031938b6a413b67cb47e
bbasstyle <bbasstyle@gmail.com>
2019-04-20 14:17:08 -0400

    commit inicial

---
+++ src/main/java/cl/bastian/java/pollos/App.java
@@ -0,0 +1,19 @@
+package cl.bastian.java.pollos;
+
+/**
+ * Hello world!
+ * Clase de ejemplo inicial para canal java para pollos.
+ * @author bastian
+ * 
+ */
+public class App {
+	/**
+	 * mètodo inicial con mesaje hola mundo.
+	 * 
+	 * @param args parametros en casos de ser necesarios.
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo (java para pollos)!");
+	}
+}
